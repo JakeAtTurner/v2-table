1947b45c917dd770548cdde02c9b688825496fd8
Jacob Cukjati <jakecukjati@gmail.com>
2018-04-06 10:15:38 -0400

    AM-758 faster table rendering

---
+++ src/components/table-row-hovered-section.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="row_hover_overlay">
+      <div :style="nameStyle" >
+      </div>
+      <!-- The row_hover_overlay__overLayPortion is the custom CSS for the background color -->
+      <div class="row_hover_overlay__overLayPortion" :style="overlayStyle">
+          <div class="table-hover-over-background row_hover_overlay__background"></div>
+          <slot></slot>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    nameStyle: Object,
+    overlayStyle: Object
+  }
+}
+</script>
+
+<style>
+.table-hover-over-background {
+    position: absolute !important;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1000;
+}
+</style>
