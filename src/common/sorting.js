a38c37ed0aa597455fb93e81e6cb7dbb91eab81f
Jacob Cukjati <jakecukjati@gmail.com>
2018-05-01 10:38:43 -0400

    fix: fixed the eslint configs

--- src/common/sorting.js
+++ src/common/sorting.js
@@ -1,63 +1,63 @@
-import {getAttribute} from './util'
+import { getAttribute } from './util';
 
 const createNumberSort = function (prop) {
-  return (a,b) => {
-    let numA = getAttribute(a, prop)
-    let numB = getAttribute(b, prop)
-    return numA - numB
-  }
-}
+    return (a, b) => {
+        const numA = getAttribute(a, prop);
+        const numB = getAttribute(b, prop);
+        return numA - numB;
+    };
+};
 
 const createStringSort = function (prop) {
-  return (a,b) => {
-    let stringA = getAttribute(a, prop)
-    let stringB = getAttribute(b, prop)
-    if (stringA && stringB) {
-      stringA = stringA.toLowerCase()
-      stringB = stringB.toLowerCase()
-      if (stringA < stringB) {
-        return -1
-      } else if (stringA > stringB) {
-          return 1
-      } else {
-          return 0
-      }
-    } else {
-      if (stringA) {
-        return -1
-      } else if (stringB) {
-        return 1
-      } else {
-        return 0
-      }
-    }
-  }
-}
+    return (a, b) => {
+        let stringA = getAttribute(a, prop);
+        let stringB = getAttribute(b, prop);
+        if (stringA && stringB) {
+            stringA = stringA.toLowerCase();
+            stringB = stringB.toLowerCase();
+            if (stringA < stringB) {
+                return -1;
+            } else if (stringA > stringB) {
+                return 1;
+            } else {
+                return 0;
+            }
+        } else {
+            if (stringA) {
+                return -1;
+            } else if (stringB) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    };
+};
 
 const createSortFunction = function (prop, order, type) {
-  let sortingFunc = null
-  if (type === Number) {
-    sortingFunc = createNumberSort(prop)
-  } else if (type === String) {
-    sortingFunc = createStringSort(prop)
-  }
-  sortingFunc = applyDescending(order, sortingFunc)
-  return sortingFunc
-}
+    let sortingFunc = null;
+    if (type === Number) {
+        sortingFunc = createNumberSort(prop);
+    } else if (type === String) {
+        sortingFunc = createStringSort(prop);
+    }
+    sortingFunc = applyDescending(order, sortingFunc);
+    return sortingFunc;
+};
 
 const applyDescending = function (order, sortingFunc) {
-  let isDescending = order !== 'ascending'
-  if (isDescending) {
-    let descendingFunc = (a,b) => {
-        let v = sortingFunc(a,b)
-        return v * -1
+    const isDescending = order !== 'ascending';
+    if (isDescending) {
+        const descendingFunc = (a, b) => {
+            const v = sortingFunc(a, b);
+            return v * -1;
+        };
+        return descendingFunc;
+    } else {
+        return sortingFunc;
     }
-    return descendingFunc
-  } else {
-    return sortingFunc
-  }
-}
+};
 
 export {
-  createSortFunction
-}
+    createSortFunction
+};
